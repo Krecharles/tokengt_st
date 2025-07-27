c4c786fc5ef42358a1af882910df5717ccfe9056
Krecharles <charelctk@gmail.com>
2025-07-27 19:15:59 +0200

    up

--- tokengt_experiments/qm9/qm9_dataset.py
+++ tokengt_experiments/qm9/qm9_dataset.py
@@ -1,12 +1,15 @@
+from typing import List, Optional
+
 import pytorch_lightning as pl
+import torch
 from torch.utils import data
 from torch_geometric.datasets import QM9
-from torch_geometric.transforms import Compose
-from torch_geometric.transforms.add_tokengt_node_identifiers import AddLaplacianNodeIdentifiers, AddPrecomputedORFNodeIdentifiers
 from torch_geometric.loader import DataLoader
-from typing import List, Optional
-import torch
-from models.add_smarts_instances import AddSubstructureEmbeddings, get_qm9_smarts_patterns, AddSmartsInstances
+from torch_geometric.transforms import Compose
+
+from models.add_smarts_instances import AddSmartsInstances, AddSubstructureEmbeddings
+from tokengt_paper_repo.wrapper import AddTokenGTPaperNodeIdentifiers
+
 
 class QM9Dataset(pl.LightningDataModule):
     def __init__(
@@ -18,6 +21,7 @@ class QM9Dataset(pl.LightningDataModule):
         data_dir: str = "data",
         smarts_patterns: List[List[str]] = [],
         embed_smarts: bool = False,
+        target_idx: int = 0,
     ):
         super().__init__()
         self.batch_size = batch_size
@@ -27,23 +31,22 @@ class QM9Dataset(pl.LightningDataModule):
         self.data_dir = data_dir
         self.smarts_patterns = smarts_patterns
         self.embed_smarts = embed_smarts
-        
+        self.target_idx = target_idx
+
         flatten = lambda lst: [item for sublist in lst for item in sublist]
         self.root_f = f"{self.data_dir}/qm9_{node_id_mode}_{d_p}_{embed_smarts}_{'_'.join(flatten(self.smarts_patterns))}"
         
         self.transform = self.get_transforms()
 
     def get_transforms(self) -> Compose:
-        transforms = []
-        if self.node_id_mode == "laplacian":
-            transforms.append(AddLaplacianNodeIdentifiers(self.d_p))
-        elif self.node_id_mode == "precomputed":
-            transforms.append(AddPrecomputedORFNodeIdentifiers(self.d_p))
-        
-        transforms.append(AddSmartsInstances(self.smarts_patterns))
-
-        if self.embed_smarts:
-            transforms.append(AddSubstructureEmbeddings(len(self.smarts_patterns)))
+        transforms = [
+            FormatQM9Features(self.target_idx),
+            AddTokenGTPaperNodeIdentifiers(self.d_p, convert_to_single_emb_offset=10),
+        ]
+        if len(self.smarts_patterns) > 0:
+            transforms.append(AddSmartsInstances(self.smarts_patterns))
+            if self.embed_smarts:
+                transforms.append(AddSubstructureEmbeddings(len(self.smarts_patterns)))
 
         return Compose(transforms)
 
@@ -83,3 +86,14 @@ class QM9Dataset(pl.LightningDataModule):
             persistent_workers=True,
             pin_memory=True
         )
+
+class FormatQM9Features(object):
+    def __init__(self, target_idx):
+        self.target_idx = target_idx
+
+    def __call__(self, data):
+        # only keep atom number and number of hydrogens
+        relevant_features = torch.tensor([5, 10])
+        data.x = data.x[:, relevant_features]
+        data.y = data.y[:, self.target_idx]
+        return data
